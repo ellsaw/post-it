6f7ea0864374ecddb6fccfb4359735dd7f7697e0
Elliot Sand-Warge <elliotsandmail@gmail.com>
2024-10-22 17:13:13 +0200

    first working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-const postItData = JSON.parse(localStorage.getItem(`postIt${localStorage.length}`));
+import { postItFilter, postItStorageLength } from "./recieveData.js";
+
+
+function postItObjectifyer(){
+    let object = {};
+    for(let i = 1; i <= postItStorageLength(); i++){
+        object[`postIt${i}`] = JSON.parse(postItFilter()[`postIt${i}`]);
+    }
+    return object;
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const main = document.getElementById("main");
+
+    function HTMLApplyer(){
+        const postItObject = postItObjectifyer();
+        for(let i = 1; i <= postItStorageLength(); i++){
+            main.insertAdjacentHTML("beforeend",
+        `
+        <article id="postit${i}" class="postIt postItColorYellow">
+            <div class="postItHeader">
+                <h2 class="postItTitle">${postItObject[`postIt${i}`].postItHeaderInput}</h2>
+                <div class="deleteAndEdit">
+                    <button class="postItEdit"><i class="bi bi-pen"></i></button>
+                    <button class="postItDelete"><i class="bi bi-trash3"></i></button>
+                </div>
+            </div>
+            <div class="postItBody">
+                <p>${postItObject[`postIt${i}`].postItBodyInput}</p>
+            </div>
+        </article>
+        `);
+        }
+    }
+    HTMLApplyer();
+})
--- js/recieveData.js
+++ js/recieveData.js
@@ -1,11 +1,26 @@
 function storeData(data){
-    localStorage.setItem(`postIt${localStorage.length+1}`, JSON.stringify(data));
+    const length = postItStorageLength();
+    if(length){
+        localStorage.setItem(`postIt${length + 1}`, JSON.stringify(data));
+    }else{
+        localStorage.setItem(`postIt1`, JSON.stringify(data));
+    }
     refreshPage();
 }
 
 function refreshPage(){
     location.reload();
 };
+
+export function postItStorageLength(){
+    return Object.keys(postItFilter()).length;
+}
+
+export function postItFilter(){
+    return Object.fromEntries(
+        Object.entries(localStorage).filter(([key]) => key.startsWith("postIt"))
+    )
+}
 
 document.addEventListener("DOMContentLoaded", () =>{
     const form = document.querySelector(".addPostItForm");
